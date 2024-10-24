b8e1cbf385372ba051e9a5d346f519360f70b99d
rayanhaq <rhaq@loyola.edu>
2024-10-24 13:00:25 -0400

    code

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 # Data Out:  [What information do you display for the user?]
 # Credits: [Is your code based of an example in the book, in class, or something else?  Reminder: you should never take code from the Internet or another person.]
 # Display the purpose of the program
+from symbol import return_stmt
 
 HARDWOOD = 1.39
 CARPET = 3.99
@@ -17,4 +18,5 @@ def calc_room_area (room_width, room_length):
     floor_area = room_length * room_width
     return floor_area
 def calc_room_floor_cost (floor_area, floor_type):
-    floor_area * floor_type
+    cost = floor_area * floor_type
+    return cost
