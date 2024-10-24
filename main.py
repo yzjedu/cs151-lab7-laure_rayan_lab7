68557342b5aa5bb7a5f2a871a067c65db0e4fa07
LaurePatera <ampatera@loyola.edu>
2024-10-24 12:54:16 -0400

    updated main.py

--- main.py
+++ main.py
@@ -7,3 +7,14 @@
 # Data Out:  [What information do you display for the user?]
 # Credits: [Is your code based of an example in the book, in class, or something else?  Reminder: you should never take code from the Internet or another person.]
 # Display the purpose of the program
+
+HARDWOOD = 1.39
+CARPET = 3.99
+TILE = 4.99
+
+
+def calc_room_area (room_width, room_length):
+    floor_area = room_length * room_width
+    return floor_area
+def calc_room_floor_cost (floor_area, floor_type):
+    floor_area * floor_type
